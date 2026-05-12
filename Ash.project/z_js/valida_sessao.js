b1c37746cfb34b84bb795b93d066b19e187c9449
Mateus Canatto <mafaam01@gmail.com>
2026-05-12 09:47:48 -0300

    Adicionando Visualizar horas complementares

--- Ash.project/z_js/valida_sessao.js
+++ Ash.project/z_js/valida_sessao.js
@@ -1,5 +1,5 @@
 async function valida_sessao(){
-    const retorno = await fetch("../php/valida_sessao.php");
+    const retorno = await fetch("../../z_php/valida_sessao.php");
     const resposta = await retorno.json();
     if(resposta.status == "nok"){
         window.location.href = '../login/';
