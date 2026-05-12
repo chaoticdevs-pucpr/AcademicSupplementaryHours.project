fafcdfb376b18217287e3ed98ab281169ecb3d29
danielschmetterling <daniel.schmetterling@proton.me>
2026-05-12 20:03:36 -0300

    Arrumando nome

--- Ash.project/pages/estudante/index.js
+++ Ash.project/pages/estudante/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-   // valida_sessao('ESTUDANTE');
+    valida_sessao();
 
     // MUDANÇA AQUI: de "solicitacoes" para "btn-solicitacoes"
     const btnSolicitacoes = document.getElementById("solicitacoes"); 
